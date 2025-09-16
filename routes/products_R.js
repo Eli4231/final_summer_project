9aebf22dd3cf9bcc031b65bcef96f1f308f0ae67
Eli lang <elilang92@gmail.com>
2025-09-16 23:53:31 +0300

    fs+multer

--- routes/products_R.js
+++ routes/products_R.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const multer =require('multer');
+const path = require('path');
+const fs = require('fs');
 
-let products = [];
+let projects = [];
 let nextID =1;
 
-router.get('/',(req,res)=>{
-    res.json(products)
+if(!fs.existsSync('images')){
+    fs.mkdirSync('images');
+}
+const storage =multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'images/');
+    },
+    filename: (req,file,cb)=>{
+        let id =req.params.id ? req.params.id : nextId;
+        let finalFileName= `${id}${path.extname(file.originalname)}`;
+        cb(null,finalFileName);
+    }
 });
+
+router.get('/',(req,res)=>{
+    res.json(projects);
+});
+
+router.post('/',(req,res)=>{
+   let name =req.body.name;
+    id=nextID;
+    let project ={id,name};
+    projects[id]=project;
+    res.json({massege:"ok"});
+
+});
+
+
 
 module.exports =router;
