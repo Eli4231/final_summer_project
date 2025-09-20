1c86b4b881467dc93a3a52b3ee7dd7e324292bea
Eli lang <elilang92@gmail.com>
2025-09-20 17:01:16 +0300

    התחלנו צד לקוח פונקציית getData

--- public/script.js
+++ public/script.js
@@ -2,4 +2,25 @@ function addTitle(){
     let txt = "projects";
     document.getElementById("h1").innerText=txt;
 }
+
+async function getData() {
+    try {
+        let response = await fetch ('/p');
+        let data = await response.json();
+        createGrid(data);
+    } catch (error) {
+        alert (error)
+    }
+}
+
+function createGrid(data){
+  let txt= "";
+
+}
+
+
+
+
+
+
 addTitle();
